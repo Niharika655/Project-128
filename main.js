a5765365f75c536ebcd5f374e9b89390b15a975f
Niharika655 <73527574+Niharika655@users.noreply.github.com>
2021-09-09 16:29:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+lefxtWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+    song1 = loadSound("song1.mp3");
+    song2 = loadSound("song2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+function gotPoses(results) {
+    console.log(results);
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
